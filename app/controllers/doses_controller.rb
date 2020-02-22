b1c5e4931cc7e48b3bc8b4fcc3dabffba0e652af
Dominic Ferris <ferris.dominic@gmail.com>
2020-02-22 23:10:43 +0000

    refactor doses controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,11 @@
 class DosesController < ApplicationController
+  before_action :find_cocktail, only: [:new, :create]
+
   def new
     @dose = Dose.new
-    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
@@ -25,5 +25,9 @@ class DosesController < ApplicationController
 
   def dose_params
     params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
